5c83c1f6082165c03f68fdd16e811f7f234998aa
rodmcnew <rodmcnew@gmail.com>
2016-02-23 12:26:53 -0600

    speed up filter by 1.75 ms :/

--- public/domain-filter.js
+++ public/domain-filter.js
@@ -1,23 +1,23 @@
 /**
  * Filter that will query all existing domains
  */
-angular.module('rcmRedirectEditor').filter('domainFilter', function () {
-    return function (input, query) {
-        if (!query) {
-            return input
-        }
-        var result = {};
-        var regex = new RegExp(query, 'i');
-        angular.forEach(
-            input, function (site) {
-                if (site.domain && site.domain.search(regex) !== -1) {
-                    result[site.siteId] = site;
-                }
+angular.module('rcmRedirectEditor')
+    .filter('domainFilter', function () {
+        return function (input, query) {
+            if (!query) {
+                return input
             }
-        );
-
-        return result;
-    };
-});
+            var result = {};
+            var regex = new RegExp(query, 'i');
+            angular.forEach(
+                input, function (site) {
+                    if (site.domain && regex.test(site.domain)) {
+                        result[site.siteId] = site;
+                    }
+                }
+            );
+            return result;
+        };
+    });
 
 
