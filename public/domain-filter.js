b9d54edeecb54045a7dd845143707927f5c593a4
rodmcnew <rodmcnew@gmail.com>
2016-02-23 12:10:10 -0600

    speed up? maybe?

--- public/domain-filter.js
+++ public/domain-filter.js
@@ -2,30 +2,23 @@
  * Filter that will query all existing domains
  */
 angular.module('rcmRedirectEditor')
-.filter('domainFilter', function () {
-
-    var compareStr = function (stra, strb) {
-        stra = ("" + stra).toLowerCase();
-        strb = ("" + strb).toLowerCase();
-
-        return stra.indexOf(strb) !== -1;
-    };
-
-    return function (input, query) {
-        if (!query) {
-            return input
-        }
-        var result = {};
-        angular.forEach(
-            input, function (site) {
-                if (compareStr(site.domain, query)) {
-                    result[site.siteId] = site;
-                }
+    .filter('domainFilter', function () {
+        return function (input, query) {
+            if (!query) {
+                return input
             }
-        );
+            var result = {};
+            var regex = new RegExp(query, 'i');
+            angular.forEach(
+                input, function (site) {
+                    if (site.domain && site.domain.search(regex) !== -1) {
+                        result[site.siteId] = site;
+                    }
+                }
+            );
 
-        return result;
-    };
-});
+            return result;
+        };
+    });
 
 
