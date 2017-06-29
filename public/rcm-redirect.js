b3d36321f873610edb82657e0a023f556d1507b2
James Jervis <jjervis@relivinc.com>
2017-06-29 17:29:35 -0500

    Update

--- public/rcm-redirect.js
+++ public/rcm-redirect.js
@@ -4,7 +4,7 @@
 /**
  * rcmRedirectEditor module
  */
-angular.module('rcmRedirectEditor', []);
+angular.module('rcmRedirectEditor', ['rcmApiLib']);
 
 if (typeof rcm != 'undefined') {
     // RCM is undefined in unit tests
